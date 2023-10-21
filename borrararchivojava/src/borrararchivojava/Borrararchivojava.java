ab94400a672c52672349b60cfb90337c809b6ca4
luciano2777 <124527669+luciano2777@users.noreply.github.com>
2023-10-21 17:07:46 -0400

    Add files via upload

--- borrararchivojava/src/borrararchivojava/Borrararchivojava.java
+++ borrararchivojava/src/borrararchivojava/Borrararchivojava.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package borrararchivojava;
-import commons
+
 /**
  *
  * @author luciano
@@ -11,72 +11,72 @@ import commons
 
 
 
+
+ 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
- 
+import java.io.PrintWriter;
 public class Borrararchivojava{   
-    static void modifyFile(String filePath, String oldString, String newString)
-    {
-        File fileToBeModified = new File(filePath);
-         
-        String oldContent = "";
-         
-        BufferedReader reader = null;
-         
-        FileWriter writer = null;
-         
-        try
-        {
-            reader = new BufferedReader(new FileReader(fileToBeModified));
-             
-            //Reading all the lines of input text file into oldContent
-             
-            String line = reader.readLine();
-             
-            while (line != null) 
-            {
-                oldContent = oldContent + line + System.lineSeparator();
-                 
-                line = reader.readLine();
-            }
-             
-            //Replacing oldString with newString in the oldContent
-             
-            String newContent = oldContent.replaceAll(oldString, newString);
-             
-            //Rewriting the input text file with newContent
-             
-            writer = new FileWriter(fileToBeModified);
-             
-            writer.write(newContent);
+    static void modifyFile(String file, String lineToRemove)
+      {
+
+    try {
+
+      File inFile = new File(file);
+
+      if (!inFile.isFile()) {
+        System.out.println("Parameter is not an existing file");
+        return;
+      }
+
+      //Construct the new file that will later be renamed to the original filename.
+      File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+      String line = null;
+
+      //Read from the original file and write to the new
+      //unless content matches data to be removed.
+      while ((line = br.readLine()) != null) {
+
+        if (!line.trim().equals(lineToRemove)) {
+
+          pw.println(line);
+          pw.flush();
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                //Closing the resources
-                 
-                reader.close();
-                 
-                writer.close();
-            } 
-            catch (IOException e) 
-            {
-                e.printStackTrace();
-            }
-        }
+      }
+      pw.close();
+      br.close();
+
+      //Delete the original file
+      if (!inFile.delete()) {
+        System.out.println("Could not delete file");
+        return;
+      }
+
+      //Rename the new file to the filename the original file had.
+      if (!tempFile.renameTo(inFile))
+        System.out.println("Could not rename file");
+
     }
+    catch (FileNotFoundException ex) {
+      ex.printStackTrace();
+    }
+    catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
      
     public static void main(String[] args)
     {
-        modifyFile("/home/luciano/Desktop/Universidad/4to trimestre/Java/muestra.txt", "@africa", "");
+        modifyFile("/home/luciano/Desktop/Universidad/4to trimestre/Java/muestra.txt", "@africa");
          //Oldstring es la string que será reemplazada, new string será el reemplazo.
         System.out.println("done");
     }

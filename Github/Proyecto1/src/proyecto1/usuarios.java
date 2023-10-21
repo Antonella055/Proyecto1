310926b566f2c38ce4f97badc023d77a3d121f3c
luciano2777 <mader1930@gmail.com>
2023-10-21 17:20:48 -0400

    Hola

--- Github/Proyecto1/src/proyecto1/usuarios.java
+++ Github/Proyecto1/src/proyecto1/usuarios.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package proyecto1;
-
-import java.util.ArrayList;
-import javax.swing.JFileChooser;
-
-/**
- *
- * @author Antonella
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 // */
-////class usuario { //informacion del usuario
-//        int [][] matrizConexiones =new int[numUsuarios][numUsuarios];
-//        JFileChooser listausuario=new JFileChooser();
-//        
-//        
-
-
+//package proyecto1;
+//
+//import java.util.ArrayList;
+//import javax.swing.JFileChooser;
+//
+///**
+// *
+// * @author Antonella
+//// */
+//////class usuario { //informacion del usuario
+////        int [][] matrizConexiones =new int[numUsuarios][numUsuarios];
+////        JFileChooser listausuario=new JFileChooser();
+////        
+////        
+//
+//

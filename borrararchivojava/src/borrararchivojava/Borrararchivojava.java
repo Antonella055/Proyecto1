188c5136de5330ac3fd8cad6e4a531eccecefea4
luciano2777 <124527669+luciano2777@users.noreply.github.com>
2023-10-21 00:15:35 -0400

    Modulo para borrar, añadir, modificar el archivo
    Tmbn añado una muestra de texto

---
+++ borrararchivojava/src/borrararchivojava/Borrararchivojava.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package borrararchivojava;
+import commons
+/**
+ *
+ * @author luciano
+ */
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+public class Borrararchivojava{   
+    static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+         
+        String oldContent = "";
+         
+        BufferedReader reader = null;
+         
+        FileWriter writer = null;
+         
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+             
+            //Reading all the lines of input text file into oldContent
+             
+            String line = reader.readLine();
+             
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                 
+                line = reader.readLine();
+            }
+             
+            //Replacing oldString with newString in the oldContent
+             
+            String newContent = oldContent.replaceAll(oldString, newString);
+             
+            //Rewriting the input text file with newContent
+             
+            writer = new FileWriter(fileToBeModified);
+             
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Closing the resources
+                 
+                reader.close();
+                 
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+     
+    public static void main(String[] args)
+    {
+        modifyFile("/home/luciano/Desktop/Universidad/4to trimestre/Java/muestra.txt", "@africa", "");
+         //Oldstring es la string que será reemplazada, new string será el reemplazo.
+        System.out.println("done");
+    }
+}
